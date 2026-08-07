457f69c4b8c306ec23b712bdb5cc40656f97b0c2
Arth Kamboj <arth23153015@akgec.ac.in>
2026-08-07 18:45:17 +0530

    established working connection between db and api

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://admin:securepassword123@localhost:5432/logistics"
+    DATABASE_URL: str = "postgresql://admin:securepassword123@127.0.0.1:5433/logistics"
     SECRET_KEY: str = "super_secret_jwt_key_change_in_prod"
     ALGORITHM: str = "HS256"
 
